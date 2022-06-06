638b9eb85feeb15bbb6783414a4dbe58491fa61c
Shashwat Singh (alias Ma4akaa1) <shashwatsingh71@gmail.com>
2022-06-06 21:22:44 +0530

    update

--- cf-test-1/getDet.sh
+++ cf-test-1/getDet.sh
@@ -1,3 +1,3 @@
-aws --endpoint-url=http://localhost:4566 cloudformation describe-stacks --stack-name sttest10  &> /var/lib/jenkins/workspace/cf-check-teat/cf-test/output/output.log
+aws --endpoint-url=http://localhost:4566 cloudformation describe-stacks --stack-name sttest50  &> /var/lib/jenkins/workspace/cf-check-teat/cf-test/output/output.log
 
-aws --endpoint-url=http://localhost:4566 cloudformation delete-stack --stack-name sttest10
+aws --endpoint-url=http://localhost:4566 cloudformation delete-stack --stack-name sttest50
