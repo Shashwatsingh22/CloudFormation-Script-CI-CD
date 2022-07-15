a57380d5c2d2151da2eb7188f79d84669a635d80
Shashwat SIngh <55051478+Shashwatsingh22@users.noreply.github.com>
2022-07-15 13:04:13 +0530

    Update test.sh

--- cf-test-1/test.sh
+++ cf-test-1/test.sh
@@ -1,1 +1,1 @@
-aws --endpoint-url=http://localhost:4566 cloudformation deploy --template-file ./CloudFormation-Script-CI-CD/cf-temp/cloudFormationTemp.yaml --stack-name test1  2> ./report/error.log  1> ./report/suc.log
+aws --endpoint-url=http://localhost:4566 cloudformation deploy --template-file ./CloudFormation-Script-CI-CD/cf-temp/cloudFormationTemp.yaml --stack-name test1  2> ./report/error.log  1> ./report/succ.log
