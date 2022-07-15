3b0108c2c00042621ba45e9da5763b888dfdd865
Shashwat Singh (alias Ma4akaa1) <shashwatsingh71@gmail.com>
2022-07-15 21:33:59 +0530

    completed

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,22 @@ FROM ubuntu:latest
 
 WORKDIR /home
 
-RUN apt-get update
-
-RUN apt install git -y; apt install python3 -y
-
 #Copy all files
 COPY . .
+
+RUN apt-get update
+
+RUN apt install git -y; apt install python3 -y; apt install curl -y;apt install unzip -y
+
+RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+RUN unzip awscliv2.zip
+
+RUN ./aws/install
+
+RUN aws configure list
 
 #Execute the Shell Script
 RUN chmod +x main.sh
 
-#ENTRYPOINT ["./main.sh"]
+ENTRYPOINT ["./main.sh"]
---
+++ main.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+echo "Stage - 2 = Lets Start Testing"
+
+if (bash ./cf-test-1/test.sh); then
+
+	echo "Cf Script Successfully Fine"
+
+	echo "Stage -3 = Send Succ Mail to the User"
+
+	#First Get the Detail of Deployment and also get the delete the Stack
+
+	(bash ./cf-test-1/getDet.sh)
+
+	# Sending the Mail
+
+	if (python3 sendMail.py no-error-found); then
+
+		echo "Successfully Able to Send the Mail"
+
+	else
+		echo "Some Error Caused While Sending the Mail"
+	
+	fi
+
+	# Push to Main Branch
+
+	echo "Stage - 4 = Push to Main Branch"
+
+	if (echo "Push to Main Branch"); then
+
+		echo "Successfully Able to Push to Main Branch"
+
+	else 
+		
+		echo "Some Error Caused While pushing to Main Branch"
+
+	fi	
+
+
+else
+	echo "Cf Script have some problem"
+
+	echo "Stage - 3 & 4 =  Skipped"
+
+	echo "Stage - 5 = Sending the Error Mail to Cloud Engineer"
+
+	if (python3 sendMail.py error-found); then
+
+		echo "Error Mailed Successfully"
+	
+	else
+		echo "Some Error Caused While Sending the Mail"
+	
+	fi	
+
+fi
