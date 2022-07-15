4ffc06079877d391d486a3eeeee4877e4e174eb7
Shashwat Singh (alias Ma4akaa1) <shashwatsingh71@gmail.com>
2022-07-15 15:41:54 +0530

    test4

--- cf-test-1/getDet.sh
+++ cf-test-1/getDet.sh
@@ -1,3 +1,3 @@
-aws --endpoint-url=http://localhost:4566 cloudformation describe-stacks --stack-name test3  &> ./CloudFormation-Script-CI-CD/report/output.log
+aws --endpoint-url=http://localhost:4566 cloudformation describe-stacks --stack-name test4  &> ./CloudFormation-Script-CI-CD/report/output.log
 
-aws --endpoint-url=http://localhost:4566 cloudformation delete-stack --stack-name test3
+aws --endpoint-url=http://localhost:4566 cloudformation delete-stack --stack-name test4
