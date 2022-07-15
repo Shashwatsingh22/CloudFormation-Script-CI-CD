d47fc7d40f85fa20fd7d4948af9c700e6a60530f
Shashwat SIngh <55051478+Shashwatsingh22@users.noreply.github.com>
2022-07-15 18:12:30 +0530

    Update getDet.sh

--- cf-test-1/getDet.sh
+++ cf-test-1/getDet.sh
@@ -1,3 +1,3 @@
-aws --endpoint-url=http://localhost:4566 cloudformation describe-stacks --stack-name test4  &> ./CloudFormation-Script-CI-CD/report/output.log
+aws --endpoint-url=http://localhost:4566 cloudformation describe-stacks --stack-name test4  &> ./report/output.log
 
 aws --endpoint-url=http://localhost:4566 cloudformation delete-stack --stack-name test4
