fe8952c29cbc16df70ddeba8afbd319dda7efe7b
Shashwat Singh (alias Ma4akaa1) <shashwatsingh71@gmail.com>
2022-07-15 22:17:37 +0530

    test5

--- cf-test-1/getDet.sh
+++ cf-test-1/getDet.sh
@@ -1,3 +1,3 @@
-aws --endpoint-url=$(END-POINT) cloudformation describe-stacks --stack-name test4  &> ./report/output.log
+aws --endpoint-url=http://13.233.103.240:4566 cloudformation describe-stacks --stack-name test5  &> ./report/output.log
 
-aws --endpoint-url=$(END-POINT) cloudformation delete-stack --stack-name test4
+aws --endpoint-url=http://13.233.103.240:4566 cloudformation delete-stack --stack-name test5
--- cf-test-1/test.sh
+++ cf-test-1/test.sh
@@ -1,1 +1,1 @@
-aws --endpoint-url=$(END-POINT) cloudformation deploy --template-file ./cf-temp/cloudFormationTemp.yaml --stack-name test4  2> ./report/error.log  1> ./report/succ.log
+aws --endpoint-url=http://13.233.103.240:4566 cloudformation deploy --template-file ./cf-temp/cloudFormationTemp.yaml --stack-name test5  2> ./report/error.log  1> ./report/succ.log
